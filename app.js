d664fccbcb2f3c456fa3d2f7be1e57ab1ccce2be
MUHAMMED YOUNUS ATTARI <mohammedyounus999@gmail.com>
2017-10-27 08:50:45 +0530

    nodejs + expressjs example

--- app.js
+++ app.js
@@ -1,7 +1,31 @@
-var http = require("http");
-http.createServer(function(request,response){
-    response.end("HelloWorld...");
-    response.end("Hello Nodejs");
-}).listen("8181");
-console.log("server is running on port 8181, you access it by clicking <a href='http://localhost:8181'>Server Access</a>");
- 
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+
+
+app.get('/',function(request,response){
+    response.send("<h1>Hello World</h1>")
+});
+app.get('/login.html',function(request,response){
+    // response.send('<h1>Login</h1>');
+    console.log(__dirname);
+    response.sendFile(__dirname+"/"+"login.html");
+});
+
+app.get('/register.html',function(request,response){
+    response.sendFile(__dirname+"/"+"register.html");
+})
+
+app.post('/loginUser',urlencodedParser,function(request,response){
+
+    console.log(request.body);
+    var uname = request.body.uname;
+    var pwd = request.body.pwd;
+    if(uname==pwd)
+        response.sendFile(__dirname+"/"+"success.html");
+    else           response.sendFile(__dirname+"/"+"error.html");
+
+});
+app.listen('8080');
